88603e3ed121d64746e92e0419d23215d839fffe
LeoFlik <leo2071@gmail.com>
2023-12-04 19:19:05 -0300

    'the morse data is now a dictionay, now it's time to set the input

--- morsenv/main.py
+++ morsenv/main.py
@@ -1,1 +1,7 @@
+from morse_code import morse_dict
 
+
+# user_input = input("Type your message to convert: ").upper()
+
+
+# print(user_input)
--- morsenv/morse_code.py
+++ morsenv/morse_code.py
@@ -16,4 +16,15 @@ df = pd.read_html(morse_data)
 my_list = [table for table in df]
 word_list = my_list[0]
 
-print(word_list.describe())
+morse_code = word_list[["Character", "Morse Code"]]
+
+morse_code = morse_code.set_index("Character")
+
+
+morse_code = morse_code[:26]
+
+data = morse_code.to_dict()
+
+morse_dict = data["Morse Code"]
+
+print(morse_dict)
