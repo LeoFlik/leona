21e6519833a6a99fa6c272160476765789268812
LeoFlik <leo2071@gmail.com>
2023-12-04 20:37:19 -0300

    testinn the logic for incode function

--- morsenv/main.py
+++ morsenv/main.py
@@ -1,7 +1,10 @@
 from morse_code import morse_dict
 
+user_input = input("Type your message to convert: ").upper()
 
-# user_input = input("Type your message to convert: ").upper()
 
-
-# print(user_input)
+for item in user_input:
+    if item in morse_dict:
+        code = item
+        new = "".join(code)
+        print(new)
--- morsenv/morse_code.py
+++ morsenv/morse_code.py
@@ -7,24 +7,21 @@ morse_code_request = rq.get(
     "https://morsedecoder.com/morse-code-alphabet/#international"
 )
 
-print(morse_code_request.status_code)
 
+# before reading the html file, now is necessary parse the data in this IO format
 morse_data = io.StringIO(morse_code_request.text)
 
 df = pd.read_html(morse_data)
 
 my_list = [table for table in df]
+
 word_list = my_list[0]
 
+# cleaning the data, I've got only the words/code data
 morse_code = word_list[["Character", "Morse Code"]]
 
+# cleaning the index and setting the columns values
 morse_code = morse_code.set_index("Character")
 
-
-morse_code = morse_code[:26]
-
-data = morse_code.to_dict()
-
-morse_dict = data["Morse Code"]
-
-print(morse_dict)
+# dict ready to export to main file
+morse_dict = morse_code["Morse Code"]
