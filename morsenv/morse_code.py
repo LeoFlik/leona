5c8171b4e541dbac4fc2c110a77999e56185bae8
LeoFlik <leo2071@gmail.com>
2023-12-04 18:15:01 -0300

    change the font of my word list

--- morsenv/morse_code.py
+++ morsenv/morse_code.py
@@ -1,5 +1,19 @@
 import pandas as pd
 import requests as rq
+import io
 
 
-morse_code_request = rq.get("https://morsecode.world/international/morse2.html")
+morse_code_request = rq.get(
+    "https://morsedecoder.com/morse-code-alphabet/#international"
+)
+
+print(morse_code_request.status_code)
+
+morse_data = io.StringIO(morse_code_request.text)
+
+df = pd.read_html(morse_data)
+
+my_list = [table for table in df]
+word_list = my_list[0]
+
+print(word_list.describe())
